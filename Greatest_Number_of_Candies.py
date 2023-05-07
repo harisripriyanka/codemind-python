e8fda14e25f0ffa7331ddd6c00f77b17bf81dd25
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-05-07 20:33:25 +0530

    n = int(input()) lst = list(map(int,input().split())) ex = int(input()) for i in lst:     k = i+ex     if max(lst)<=k:         print(True,end=' ')     else:         print(False,end=' ')

---
+++ Greatest_Number_of_Candies.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int,input().split()))
+ex = int(input())
+for i in lst:
+    k = i+ex
+    if max(lst)<=k:
+        print(True,end=' ')
+    else:
+        print(False,end=' ')
