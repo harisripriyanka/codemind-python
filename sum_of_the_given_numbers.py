cb7c5ffc690f37de103e1889481ff255228750c2
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-05-16 21:45:10 +0530

    n = int(input()) for i in range(n):     a,b = map(int,input().split())     sum = a+b     print(sum)

--- sum_of_the_given_numbers.py
+++ sum_of_the_given_numbers.py
@@ -1,7 +1,5 @@
-def add_num():
-    n = int(input())
-    for i in range(n):
-        a,b = map(int,input().split())
-        sum = a+b
-        print(sum)
-add_num()
+n = int(input())
+for i in range(n):
+    a,b = map(int,input().split())
+    sum = a+b
+    print(sum)
