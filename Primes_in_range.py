44ee81316787f909cb74f8935f86baffe5ca3004
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-06-02 15:41:31 +0530

    def pr(n):     if(n==1):         return 0     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return 0                 break     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if(pr(i)==1):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,6 +1,6 @@
-def prime(n):
+def pr(n):
     if(n==1):
-        return 0
+        return 0 
     else:
         for i in range(2,int(n**0.5)+1):
             if n%i==0:
@@ -11,6 +11,6 @@ a=int(input())
 b=int(input())
 c=0
 for i in range(a,b+1):
-    if(prime(i)==1):
+    if(pr(i)==1):
         c+=1
 print(c)
